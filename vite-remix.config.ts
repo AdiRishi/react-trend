5e6f83811bd31753102c23a338375a8226811b5b
Adishwar Rishi <adiswa123@gmail.com>
2024-04-19 12:41:39 +1000

    Add vite plugin visualizer

--- vite-remix.config.ts
+++ vite-remix.config.ts
@@ -1,3 +1,4 @@
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { vitePlugin as remix } from '@remix-run/dev';
@@ -6,9 +7,11 @@ export default defineConfig({
   publicDir: 'demo-app/public',
   plugins: [
     remix({
+      future: { v3_fetcherPersist: true, v3_relativeSplatPath: true, v3_throwAbortReason: true },
       appDirectory: 'demo-app',
       ssr: false,
     }),
     tsconfigPaths(),
+    visualizer({ emitFile: true }),
   ],
 });
