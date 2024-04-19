59e04262c2116330c7bd99ca27d5164356ea20ee
Adishwar Rishi <adiswa123@gmail.com>
2024-04-19 12:33:08 +1000

    Setup demo in remix

--- demo-app/routes/_index.tsx
+++ demo-app/routes/_index.tsx
@@ -1,6 +1,11 @@
-import { Trend } from '../lib';
+import { Trend } from '@lib/index';
+import type { MetaFunction } from '@remix-run/node';
 
-function App() {
+export const meta: MetaFunction = () => {
+  return [{ title: 'New Remix SPA' }, { name: 'description', content: 'Welcome to Remix (SPA Mode)!' }];
+};
+
+export default function Index() {
   const placeholderData = [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0];
   const gradient = ['#00c6ff', '#F0F', '#FF0'];
   const radius = 10;
@@ -26,5 +31,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
