e56dc5e611bc2dff6d052ecaf42fa24ea7d0c915
Adishwar Rishi <adiswa123@gmail.com>
2024-05-04 16:44:04 +1000

    Add github pages config

--- vite-remix.config.ts
+++ vite-remix.config.ts
@@ -5,8 +5,10 @@ import { vitePlugin as remix } from '@remix-run/dev';
 
 export default defineConfig({
   publicDir: 'demo-app/public',
+  base: '/react-trend/',
   plugins: [
     remix({
+      basename: '/react-trend/',
       future: { v3_fetcherPersist: true, v3_relativeSplatPath: true, v3_throwAbortReason: true },
       appDirectory: 'demo-app',
       ssr: false,
